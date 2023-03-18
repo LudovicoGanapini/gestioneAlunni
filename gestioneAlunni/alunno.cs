fcbc3949598ac61d4620a2052952c28e7b8e7d0f
Ludovico Mario Ganapini <LUDOVICO.GANAPINI@local.ittcesena.it>
2023-03-18 08:23:11 +0100

    Versione 1. aggiunti attributi classe alunno

---
+++ gestioneAlunni/alunno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestioneAlunni
+{
+    internal class alunno
+    {
+        string nome;
+        string cognome;
+        int età;
+        string città;
+
+
+
+    }
+}
